9f77b4eba7a9d97e20ebe44a02a4d1fb09dc3826
panyakorn04 <panyakorn40@gmail.com>
2022-06-20 11:13:11 +0700

    fitst commit

---
+++ components/Layouts/Layout.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from '@/components/Layouts/Header';
+import Menu from '@/components/Layouts/Menu';
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <Header open={open} onDrawerOpen={() => setOpen(true)} />
+            <Menu open={open} onDrawerOpen={() => setOpen(false)} />
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <DrawerHeader />
+                {children}
+            </Box>
+        </Box>
+    );
+}
